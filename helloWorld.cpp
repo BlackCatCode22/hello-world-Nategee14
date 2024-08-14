369aa982f77194d1c0dfd02cc377dee6aac5713e
Nategee14 <nategee91@gmail.com>
2024-08-14 11:55:40 -0700

    Add files via upload
    uploaded my first C++ program.

---
+++ helloWorld.cpp
@@ -0,0 +1,15 @@
+// Nate 8/14/24
+// helloWorld.cpp
+/*
+ * this is my first C++program
+ * it only took me an hour to write!
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\nHello, World! " << "another string" << endl;
+    return 0;
+}
